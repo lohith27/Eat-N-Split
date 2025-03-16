ffbf8e9bfa13e477c5a2d8318217eec19da52b91
lohith27 <lohithkumar1997@gmail.com>
2025-03-16 22:49:34 +0530

    Fixed state resetting
    Fixed state resetting

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ export default function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
